348389d128600f98e6d9ec50819130822731eecb
geina corrente <33171782+geina17@users.noreply.github.com>
2018-03-30 16:25:25 -0700

    Add files via upload

--- liris.js
+++ liris.js
@@ -10,8 +10,10 @@ var fs = require('fs');
 //Stored argument's array
 var nodeArgv = process.argv;
 var command = process.argv[2];
+
 //movie or song
 var x = "";
+
 //attaches multiple word arguments
 for (var i=3; i<nodeArgv.length; i++){
   if(i>3 && i<nodeArgv.length){
@@ -37,9 +39,9 @@ switch(command){
 
   case "movie-this":
     if(x){
-      omdbData(x)
+      omdbData(x);
     } else{
-      omdbData("Mr. Nobody")
+      omdbData("Mr. Nobody");
     }
   break;
 
@@ -54,20 +56,21 @@ switch(command){
 
 function showTweets(){
   //Display last 20 Tweets
-  var screenName = {screen_name: 'stefanieding'};
+  var screenName = {screen_name: 'gna_zonaGUALA'};
   client.get('statuses/user_timeline', screenName, function(error, tweets, response){
     if(!error){
       for(var i = 0; i<tweets.length; i++){
         var date = tweets[i].created_at;
-        console.log("@StefanieDing: " + tweets[i].text + " Created At: " + date.substring(0, 19));
+        console.log("@gna_zonaGUALA: " + tweets[i].text + " Created At: " + date.substring(0, 19));
         console.log("-----------------------");
         
         //adds text to log.txt file
-        fs.appendFile('log.txt', "@StefanieDing: " + tweets[i].text + " Created At: " + date.substring(0, 19));
+        fs.appendFile('log.txt', "@gna_zonaGUALA: " + tweets[i].text + " Created At: " + date.substring(0, 19));
         fs.appendFile('log.txt', "-----------------------");
       }
     }else{
       console.log('Error occurred');
+      console.log(error);
     }
   });
 }
@@ -105,31 +108,31 @@ function omdbData(movie){
 
   request(omdbURL, function (error, response, body){
     if(!error && response.statusCode == 200){
-      var body = JSON.parse(body);
+      var torso = JSON.parse(body);
 
-      console.log("Title: " + body.Title);
-      console.log("Release Year: " + body.Year);
-      console.log("IMdB Rating: " + body.imdbRating);
-      console.log("Country: " + body.Country);
-      console.log("Language: " + body.Language);
-      console.log("Plot: " + body.Plot);
-      console.log("Actors: " + body.Actors);
-      console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
-      console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+      console.log("Title: " + torso.Title);
+      console.log("Release Year: " + torso.Year);
+      console.log("IMdB Rating: " + torso.imdbRating);
+      console.log("Country: " + torso.Country);
+      console.log("Language: " + torso.Language);
+      console.log("Plot: " + torso.Plot);
+      console.log("Actors: " + torso.Actors);
+      console.log("Rotten Tomatoes Rating: " + torso.tomatoRating);
+      console.log("Rotten Tomatoes URL: " + torso.tomatoURL);
 
       //adds text to log.txt
-      fs.appendFile('log.txt', "Title: " + body.Title);
-      fs.appendFile('log.txt', "Release Year: " + body.Year);
-      fs.appendFile('log.txt', "IMdB Rating: " + body.imdbRating);
-      fs.appendFile('log.txt', "Country: " + body.Country);
-      fs.appendFile('log.txt', "Language: " + body.Language);
-      fs.appendFile('log.txt', "Plot: " + body.Plot);
-      fs.appendFile('log.txt', "Actors: " + body.Actors);
-      fs.appendFile('log.txt', "Rotten Tomatoes Rating: " + body.tomatoRating);
-      fs.appendFile('log.txt', "Rotten Tomatoes URL: " + body.tomatoURL);
+      fs.appendFile('log.txt', "Title: " + torso.Title);
+      fs.appendFile('log.txt', "Release Year: " + torso.Year);
+      fs.appendFile('log.txt', "IMdB Rating: " + torso.imdbRating);
+      fs.appendFile('log.txt', "Country: " + torso.Country);
+      fs.appendFile('log.txt', "Language: " + torso.Language);
+      fs.appendFile('log.txt', "Plot: " + torso.Plot);
+      fs.appendFile('log.txt', "Actors: " + torso.Actors);
+      fs.appendFile('log.txt', "Rotten Tomatoes Rating: " + torso.tomatoRating);
+      fs.appendFile('log.txt', "Rotten Tomatoes URL: " + torso.tomatoURL);
 
     } else{
-      console.log('Error occurred.')
+      console.log('Error occurred.');
     }
     if(movie === "Mr. Nobody"){
       console.log("-----------------------");
